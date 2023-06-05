42b35da1999d86aab6df1d9fa2c0c8171a952809
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-06-05 16:39:32 +0700

    Initial Commit

---
+++ CurrencyExchangeGBM/CurrencyExchangeGBMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeGBMApp.swift
+//  CurrencyExchangeGBM
+//
+//  Created by Steven Wijaya on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeGBMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
